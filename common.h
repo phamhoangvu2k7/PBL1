a6e2e2663188e54179f196f1c4469e99d9bddc0a
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-02-24 16:19:41 +0700

    create construct for project

---
+++ common.h
@@ -0,0 +1,34 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define MAX_PRODUCTS 100
+#define MAX_CATEGORY 20
+
+typedef struct {
+    int ngay;
+    int thang;
+    int name;
+} NgayThang;
+
+typedef struct {
+    int id;
+    char ten_thu_muc[50];
+    float thue;
+} ThuMuc;
+
+typedef struct {
+    char ma_hang[20];
+    char ten_hang[50];
+    char don_vi[20];
+    NgayThang ngay_nhap;
+    int so_luong;
+    float don_gia;
+} PhieuNhap;
+
+extern ThuMuc danh_sach_thu_muc[MAX_CATEGORY];
+extern int so_thu_muc;
+
+extern PhieuNhap danh_sach_hang[MAX_PRODUCTS];
+extern int so_hang;
+
+#endif
